6174aa2ed81d8c5401e2c478e7c43a65572049de
Mathias Verraes <mathias@verraes.net>
2020-04-30 19:41:12 +0200

    better naming of step and transform

--- src/Projections/MostPopularQuizzes.hs
+++ src/Projections/MostPopularQuizzes.hs
@@ -5,14 +5,18 @@ module Projections.MostPopularQuizzes
     ) where
 
 import           Data.List
-import qualified Data.Map.Strict as M
+import qualified Data.Map.Strict as Map
 import           EventStore
 import           Text.Printf     (printf)
 
 mostPopularQuizzes =
-    Projection {initState = M.empty, step = step', transform = transform'}
+    Projection
+        { initState = Map.empty
+        , step = trackPopularity
+        , transform = tenMostPopular
+        }
 
-type State = M.Map QuizId Quiz
+type State = Map.Map QuizId Quiz
 
 data Quiz =
     Quiz
@@ -26,17 +30,17 @@ incr quiz = quiz {popularity = popularity quiz + 1}
 
 format quiz = printf "%d: %s" (quiz |> popularity) (quiz |> quizTitle)
 
-step' :: State -> Event -> State
-step' state event = when (event |> payload)
+trackPopularity :: State -> Event -> State
+trackPopularity state event = when (event |> payload)
   where
     when QuizWasCreated {quiz_id, quiz_title} =
-        M.insert quiz_id (Quiz quiz_id quiz_title 0) state
-    when GameWasOpened {quiz_id} = M.adjust incr quiz_id state
+        Map.insert quiz_id (Quiz quiz_id quiz_title 0) state
+    when GameWasOpened {quiz_id} = Map.adjust incr quiz_id state
     when _ = state
 
-transform' :: State -> [Quiz]
-transform' state =
-    state |> M.toList |> fmap snd |> sortOn popularity |> reverse |> take 10
+tenMostPopular :: State -> [Quiz]
+tenMostPopular state =
+    state |> Map.toList |> fmap snd |> sortOn popularity |> reverse |> take 10
 
 instance Show Quiz where
     show = format
